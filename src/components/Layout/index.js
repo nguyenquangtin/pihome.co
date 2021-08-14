830a50f3a2c755e680e05abd5e2d4ee369d44975
Tony Tin Nguyen <t.nguyen@shopware.com>
2021-08-14 22:56:16 +0700

    Fix the modal video can't built

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../Fontawesome";
 import "../../assets/scss/bootstrap.scss"
 import "../../../node_modules/slick-carousel/slick/slick.css";
-import "../../../node_modules/react-modal-video/scss/modal-video.scss";
+// import "../../../node_modules/react-modal-video/scss/modal-video.scss";
 import "../../../node_modules/swiper/swiper.scss";
 import "../../assets/scss/main.scss"
 import "../../assets/scss/react-scss.scss"
